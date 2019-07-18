86a5b45d90fea4444b3643b300866a4e00e1d552
Stephen Cross <stephen.cross@oracle.com>
2019-07-18 10:00:07 -0400

    move bastion to separate config

---
+++ examples/management/access/data_sources.tf
@@ -0,0 +1,25 @@
+
+# Availability Domains
+data oci_identity_availability_domains ADs {
+  compartment_id = var.tenancy_ocid
+}
+
+# Oracle Linux VM Image
+data oci_core_images oraclelinux {
+  compartment_id = var.compartment_ocid
+
+  operating_system         = "Oracle Linux"
+  operating_system_version = "7.6"
+
+  # exclude GPU specific images
+  filter {
+    name   = "display_name"
+    values = ["^Oracle-Linux-7.6-([\\.0-9]+)-([\\.0-9-]+)$"]
+    regex  = true
+  }
+}
+
+locals {
+  availability_domain = lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name")
+}
+
