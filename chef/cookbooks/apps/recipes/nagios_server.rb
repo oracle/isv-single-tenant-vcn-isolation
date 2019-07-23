c77b421144c5ec17bb6bee12f340b5100f9e8323
Sunil Bemarkar <sunil.bemarkar@oracle.com>
2019-07-22 22:31:31 -0700

    refactoring of chef recipes

---
+++ chef/cookbooks/apps/recipes/nagios_server.rb
@@ -0,0 +1,72 @@
+#
+# Cookbook Name:: nagios-cookbook
+# Recipe:: nagios server
+# Author:: Sunil Bemarkar
+# All rights reserved 
+#
+
+
+#add user nagios
+user 'nagios' do
+	comment 'User Nagios'
+end
+
+#create group nagios and add user to this group and apache
+group 'nagcmd' do
+	comment 'Group Nagcmd'
+	members ['nagios', 'apache']
+end
+
+#create directory and set owner
+directory "/home/opc/nagios/" do
+  mode 0755
+  owner 'nagios'
+  group 'nagcmd'
+  action :create
+end
+
+####
+# download remote file nagios-4.3.4.tar.gz
+remote_file '/home/opc/nagios/nagios-4.3.4.tar.gz' do
+  source 'https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.3.4.tar.gz'
+  owner 'nagios'
+  group 'nagcmd'
+  mode '0755'
+  action :create_if_missing
+end
+unless Dir.exists?('/home/opc/nagios/nagios-4.3.4')
+
+	execute 'Extract nagios-4.3.4.tar.gz' do 
+		cwd '/home/opc/nagios'
+		command 'tar xvf nagios-4.3.4.tar.gz'
+		action :run
+	end
+
+	execute 'Before building, configure it' do 
+		cwd '/home/opc/nagios/nagios-4.3.4'
+		command './configure --with-command-group=nagcmd'
+		action :run
+	end
+
+	execute 'Compile it' do 
+		cwd '/home/opc/nagios/nagios-4.3.4'
+		command 'make all'
+		action :run
+	end
+
+	execute 'Install nagios' do 
+		cwd '/home/opc/nagios/nagios-4.3.4'
+		command 'make install && make install-commandmode'
+		action :run
+	end
+	execute 'Install init script' do 
+		cwd '/home/opc/nagios/nagios-4.3.4'
+		command 'make install-init'
+		action :run
+	end
+	execute 'Install configs' do 
+		cwd '/home/opc/nagios/nagios-4.3.4'
+		command 'make install-config && make install-webconf'
+		action :run
+	end
+end
