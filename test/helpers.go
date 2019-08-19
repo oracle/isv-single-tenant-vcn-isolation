9498bb763edc933bdb9137cba3ddea8c198f1ad0
Ionel Cerghit <ionel.cerghit@oracle.com>
2019-08-19 15:33:38 +0300

    parse tfvars file for the keys, add tfdir as parameter

--- test/helpers.go
+++ test/helpers.go
@@ -9,47 +9,66 @@ import (
 	"os"
 	"os/user"
 	"testing"
-
+	"bufio"
+	"strings"
+	"regexp"
 	"github.com/gruntwork-io/terratest/modules/ssh"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 // fetch the SSH keys for the test instance
-func getSSHKeyPair() *ssh.KeyPair {
-	// https://stackoverflow.com/questions/51918473/terraform-error-parsing-tfvars-file-when-using-the-var-file-flag
-	// look for TF_VAR_bastion_ssh_public_key_file and TF_VAR_bastion_ssh_private_key_file environment variables
-	// look for the bastion_ssh_public_key_file and bastion_ssh_private_key_file values set in examples/full-deployment/terraform.tfvars
-	// privateKeyFile := usr.HomeDir + "/.ssh/id_rsa"
-	// pubicKeyFile := usr.HomeDir + "/.ssh/id_rsa.pub"
-	var privateKeyFile, publicKeyFile string
-	var err, err2 error
+func getSSHKeyPair(terraformDirectory string) *ssh.KeyPair {
+	privateKeyFile := os.Getenv("TF_VAR_bastion_ssh_private_key_file")
+	publicKeyFile := os.Getenv("TF_VAR_bastion_ssh_public_key_file")
+
+	if len(privateKeyFile) != 0 &&  len(publicKeyFile) != 0 {
+		log.Output(1, "took bastion key files from env vars")
+	} else {
+		log.Output(1, "looking for bastion key files in tfvars file")
+		tfvarsFile := terraformDirectory + "/terraform.tfvars"
+
+		file, err := os.Open(tfvarsFile)
+		if err != nil {
+		    log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		space := regexp.MustCompile(`\s+`)
+		for scanner.Scan() {
+				line := space.ReplaceAllString(scanner.Text(), "")
+				if strings.HasPrefix(line, "bastion_ssh_private_key_file=") && len(privateKeyFile) == 0 {
+					privateKeyFile = strings.ReplaceAll(strings.Split(line, "=")[1], "\"","")
+				}
+
+				if strings.HasPrefix(line, "bastion_ssh_public_key_file=") &&  len(publicKeyFile) == 0 {
+					publicKeyFile = strings.ReplaceAll(strings.Split(line, "=")[1], "\"","")
+				}
+		}
+
+		if err := scanner.Err(); err != nil {
+		    log.Fatal(err)
+		}
+
+		if len(privateKeyFile) != 0 &&  len(publicKeyFile) != 0 {
+			log.Output(1, "took bastion key files from tfvars file")
+		}
+	}
 
 	usr, _ := user.Current()
 
-	privateKeyFile = os.Getenv("TF_VAR_bastion_ssh_private_key_file")
-	publicKeyFile = os.Getenv("TF_VAR_bastion_ssh_public_key_file")
-	if len(privateKeyFile) == 0 || len(publicKeyFile) == 0 {
-		log.Println("TF_VAR_bastion_ssh_private_key_file or TF_VAR_bastion_ssh_public_key_file environment variables not defined")
-	}
-
-	dir := os.Getenv("GOPATH") + "/src/isv-single-tenant-vcn-isolation/examples/full-deployment"
-	privateKeyFile, err = getOutputE(t, dir, "bastion_ssh_private_key_file")
-	publicKeyFile, err2 = getOutputE(t, dir, "bastion_ssh_public_key_file")
-
-	if err != nil || err2 != nil {
-		log.Println("Error parsing terraform output...")
-	}
-
-	if len(privateKeyFile) == 0 || len(publicKeyFile) == 0 {
+	if len(privateKeyFile) == 0 {
 		privateKeyFile = usr.HomeDir + "/.ssh/id_rsa"
+	}
+
+	if len(publicKeyFile) == 0 {
 		publicKeyFile = usr.HomeDir + "/.ssh/id_rsa.pub"
 	}
 
 	keyPair := ssh.KeyPair{
-		PrivateKey: readFromFile(privateKeyFile),
 		PublicKey:  readFromFile(publicKeyFile),
+		PrivateKey: readFromFile(privateKeyFile),
 	}
-
 	return &keyPair
 }
 
@@ -73,14 +92,4 @@ func getOutput(t *testing.T, terraformDirctory, key string) string {
 	}
 	value := terraform.Output(t, options, key)
 	return value
-}
-
-// OutputRequired calls terraform output for the given variable and return its value. If the value is empty, fail the test.
-func getOutputE(t *testing.T, terraformDir, key string) (string, error) {
-	options := &terraform.Options{
-		TerraformDir:    terraformDir,
-		TerraformBinary: "terraform",
-	}
-	value, err := terraform.OutputRequiredE(t, options, key)
-	return value, err
 }
