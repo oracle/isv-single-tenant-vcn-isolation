bbcc5d895ef7c19922b38b346d85a97fb61d6bd4
Bogdan Darie <bogdan.m.darie@oracle.com>
2019-08-19 13:28:09 +0300

    dynamic load SSH keypair

--- test/helpers.go
+++ test/helpers.go
@@ -16,15 +16,40 @@ import (
 
 // fetch the SSH keys for the test instance
 func getSSHKeyPair() *ssh.KeyPair {
-	// TODO get ssh keys from variables
+	// https://stackoverflow.com/questions/51918473/terraform-error-parsing-tfvars-file-when-using-the-var-file-flag
+	// look for TF_VAR_bastion_ssh_public_key_file and TF_VAR_bastion_ssh_private_key_file environment variables
+	// look for the bastion_ssh_public_key_file and bastion_ssh_private_key_file values set in examples/full-deployment/terraform.tfvars
+	// privateKeyFile := usr.HomeDir + "/.ssh/id_rsa"
+	// pubicKeyFile := usr.HomeDir + "/.ssh/id_rsa.pub"
+	var privateKeyFile, publicKeyFile string
+	var err, err2 error
+
 	usr, _ := user.Current()
-	privateKeyFile := usr.HomeDir + "/.ssh/id_rsa"
-	pubicKeyFile := usr.HomeDir + "/.ssh/id_rsa.pub"
+
+	privateKeyFile = os.Getenv("TF_VAR_bastion_ssh_private_key_file")
+	publicKeyFile = os.Getenv("TF_VAR_bastion_ssh_public_key_file")
+	if len(privateKeyFile) == 0 || len(publicKeyFile) == 0 {
+		log.Println("TF_VAR_bastion_ssh_private_key_file or TF_VAR_bastion_ssh_public_key_file environment variables not defined")
+	}
+
+	dir := os.Getenv("GOPATH") + "/src/isv-single-tenant-vcn-isolation/examples/full-deployment"
+	privateKeyFile, err = getOutputE(t, dir, "bastion_ssh_private_key_file")
+	publicKeyFile, err2 = getOutputE(t, dir, "bastion_ssh_public_key_file")
+
+	if err != nil || err2 != nil {
+		log.Println("Error parsing terraform output...")
+	}
+
+	if len(privateKeyFile) == 0 || len(publicKeyFile) == 0 {
+		privateKeyFile = usr.HomeDir + "/.ssh/id_rsa"
+		publicKeyFile = usr.HomeDir + "/.ssh/id_rsa.pub"
+	}
 
 	keyPair := ssh.KeyPair{
-		PublicKey:  readFromFile(pubicKeyFile),
 		PrivateKey: readFromFile(privateKeyFile),
+		PublicKey:  readFromFile(publicKeyFile),
 	}
+
 	return &keyPair
 }
 
@@ -48,4 +73,14 @@ func getOutput(t *testing.T, terraformDirctory, key string) string {
 	}
 	value := terraform.Output(t, options, key)
 	return value
+}
+
+// OutputRequired calls terraform output for the given variable and return its value. If the value is empty, fail the test.
+func getOutputE(t *testing.T, terraformDir, key string) (string, error) {
+	options := &terraform.Options{
+		TerraformDir:    terraformDir,
+		TerraformBinary: "terraform",
+	}
+	value, err := terraform.OutputRequiredE(t, options, key)
+	return value, err
 }
